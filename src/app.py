01619ab1c8bd97f51e5f00cbe693a245db80b297
PedroHenriqueDevBR <p.lyma18@gmail.com>
2023-03-27 23:59:10 -0300

    chore: configured statics and templates

--- src/app.py
+++ src/app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_url_path="",
+    static_folder="static",
+    template_folder="templates",
+)
+
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def login():
+    return render_template("auth/login.html")
