f1877c24b00421b4407d50e183bcf00c6c3bea27
PedroHenriqueDevBR <p.lyma18@gmail.com>
2023-04-12 23:06:27 -0300

    refact: configure pproject

--- src/main.py
+++ src/main.py
@@ -1,15 +1,25 @@
+"""Main file, init from FastAPI server."""
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
+
+class Item(BaseModel):
+    """
+    pass
+    """
+
+    name: str
+    price: float
+    in_offer: Union[bool, None] = None
+
+
 @app.get("/")
 def read_root():
-    return {
-        "Hello": "World"
-    }
-    
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    """
+    pass
+    """
+    return {"Hello": "World"}
