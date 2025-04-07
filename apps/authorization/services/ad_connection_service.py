fd9ddd15c37cef3c5887e55a4283fe1b44357e58
PedroHenriqueDevBR <p.lyma18@gmail.com>
2025-04-07 10:59:31 -0300

    refact: Active Directory Connection

--- apps/authorization/services/ad_connection_service.py
+++ apps/authorization/services/ad_connection_service.py
@@ -16,6 +16,7 @@ class ADConnectionService:
         self.address = address
         self.user_dn = user_dn
         self.user_dn_password = user_dn_password
+        self.base_dn = ""
         self.connection = None
 
     def authenticate(
@@ -24,11 +25,45 @@ class ADConnectionService:
         username: str,
         password: str,
     ) -> bool:
+        REQUIRE_CERT = ldap.OPT_X_TLS_REQUIRE_CERT  # type: ignore
+        TLS_NEVER = ldap.OPT_X_TLS_NEVER  # type: ignore
+        VERSION = ldap.VERSION3  # type: ignore
+        SCOPE = ldap.SCOPE_SUBTREE  # type: ignore
         try:
-            ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
+            ldap.set_option(REQUIRE_CERT, TLS_NEVER)
+
             self.connection = ldap.initialize(self.address)
-            self.connection.protocol_version = ldap.VERSION3
-            self.connection.simple_bind_s(username, password)
+            self.connection.protocol_version = VERSION
+            self.connection.simple_bind_s(self.user_dn, self.user_dn_password)
+
+            search_filter = f"(sAMAccountName={username})"
+            result = self.connection.search_s(
+                self.base_dn,
+                SCOPE,
+                search_filter,
+                ["distinguishedName"],
+            )
+
+            if result is None:
+                print(">>> User not found <<<")
+                raise ConnectionRefusedError()
+
+            user_founded = None
+            if len(result) != 1:
+                for item in result:
+                    if item[0] is not None:
+                        user_founded = item[0]
+                        break
+
+            if user_founded is None:
+                print(">>> User not found or multiple users found. <<<")
+                raise ConnectionRefusedError()
+
+            print(f"Found user DN: {user_founded}")
+            user_connection = ldap.initialize(self.address)
+            user_connection.protocol_version = VERSION
+            user_connection.simple_bind_s(user_founded, password)
+
             authenticator = AuthenticatorService(request=request)
             authenticator.authenticate_user(username=username)
             return True
@@ -39,10 +74,9 @@ class ADConnectionService:
             print(">>> Server down <<<")
             raise ConnectionError()
         except ldap.LDAPError as error:
-            if type(error.message) == dict and error.message.has_key("desc"):
-                print("LDAP error: " + error.message["desc"])
-            else:
-                print("LDAP error: " + error)
+            print("LDAP error: " + str(error))
+            raise ConnectionRefusedError()
+        except Exception as error:
             raise ConnectionRefusedError()
         finally:
             if self.connection is not None:
